de57991d4bc9f26747d31d506cc82e90e5c9ebcf
tomohiro ueno <hagurono9@gmail.com>
2023-06-02 07:58:23 +0900

    ファイルを更新

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,19 @@
+import 'package:afuro_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+void main() => runApp(MyApp());
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: "アフロボイス",
+      theme: ThemeData.dark(),
+      home: HomeScreen(),
     );
   }
 }
